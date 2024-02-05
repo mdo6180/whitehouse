c2e55bb585acda3e8e8ba6c2f6e10acc24241090
Minh Quan Do <mdo6180@gmail.com>
2024-02-05 17:07:32 -0500

    forgot to include some files in last commit

--- whitehouse/custom.py
+++ whitehouse/custom.py
@@ -9,11 +9,3 @@ class Template(Component):
 
     def __str__(self) -> str:
         return self.children
-
-
-class CustomComponent(Component):
-    def __init__(self, children: Union[str, 'Component', List['Component']], attributes: Dict[str, str] = {}) -> None:
-        super().__init__(children, attributes)
-    
-    def __str__(self) -> str:
-        return super().__str__()
--- whitehouse/template.py
+++
@@ -1,9 +0,0 @@
-from .base import Component
-
-
-class Template:
-    def __init__(self, component: Component) -> None:
-        self.component = component
-
-    def get_component(self) -> Component:
-        return self.component
